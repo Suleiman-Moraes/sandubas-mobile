f7ceb7a6b33b876865a7c8f023e45b80cb7bfdb3
waldeson <wdeson@hotmail.com>
2019-05-13 21:58:20 -0300

    Commit

--- src/app/pages/detalhe/detalhes/detalhes.component.ts
+++ src/app/pages/detalhe/detalhes/detalhes.component.ts
@@ -3,69 +3,70 @@ import { PageRoute, RouterExtensions } from 'nativescript-angular/router';
 import { Page } from 'tns-core-modules/ui/page/page';
 import { switchMap } from 'rxjs/operators';
 import { Mercadoria } from '~/app/shared/models/mercadoria.model';
+import { MercadoriaService } from '~/app/shared/mercadoria.service';
+import { ResponseApi } from '~/app/shared/models/Response-api.model';
+
 
 @Component({
-  selector: 'ns-detalhes',
-  templateUrl: './detalhes.component.html',
-  styleUrls: ['./detalhes.component.css'],
-  moduleId: module.id,
+    selector: 'ns-detalhes',
+    templateUrl: './detalhes.component.html',
+    styleUrls: ['./detalhes.component.css'],
+    moduleId: module.id,
 })
 export class DetalhesComponent implements OnInit {
 
-  itemId: number;
-  item: Mercadoria;
+    itemId: number;
+    item: Mercadoria;
+    qtd: number = 0;
 
-  constructor(
-    private pageRoute: PageRoute,
-    private routerExtensions: RouterExtensions,
-    private page: Page
-  ){
-    this.page.actionBarHidden = true;
-
-    this.pageRoute.activatedRoute.pipe(
-      switchMap(activatedRoute => activatedRoute.params)
-    ).forEach((params) => {
-      this.itemId = +params["id"];
-    });
-  }
-//////////////////////////////////////////////////////////////////
-  ngOnInit() {
-  }
-  toggleLike() {
-    this.item.isLike = !this.item.isLike;
-    if (this.item.isLike) {
-        this.item.likes += 1;
-    } else {
-        this.item.likes -= 1;
+    constructor(
+        private pageRoute: PageRoute,
+        private routerExtensions: RouterExtensions,
+        private page: Page,
+        private mercadoriaService: MercadoriaService
+    ){
+        this.page.actionBarHidden = true;
+        this.pageRoute.activatedRoute.pipe(
+            switchMap(activatedRoute => activatedRoute.params)
+        ).forEach((params) => {
+            this.itemId = +params["id"];
+            this.loadReasource(this.itemId);
+        });
     }
-}
 
-toggleHeart(item) {
-    item.isFavorite = !item.isFavorite;
-}
+    ngOnInit(): void{}
 
-categoryIcon() {
-    switch (this.item.categoryTag) {
-        case "Burger":
-            return String.fromCharCode(0xf0f5); //"fa-cutlery";
-            break;
-        case "Beer":
-            return String.fromCharCode(0xf0fc); //"fa-beer";
-            break;
-        case "Pancake":
-            return String.fromCharCode(0xf0f4); //"fa-coffee";
-            break;
-        case "Cake":
-            return String.fromCharCode(0xf1fd); //"fa-birthday-cake";
-            break;
-        default:
-            return String.fromCharCode(0xf06d); //"fa-fire";
-            break;
+    plus(): void{
+        this.qtd += 1;
     }
-}
 
-onCloseTap(): void {
-    this.routerExtensions.back();
-}
+    minus():void{
+        if(this.qtd > 0){
+            this.qtd -= 1;
+        }
+    }
 
+    toggleLike(): void{}
+
+    toggleHeart(item): void{}
+
+    categoryIcon(): void{}
+
+    onCloseTap(): void{
+        this.routerExtensions.back();
+    }
+
+    private loadReasource(id:number){
+        this.mercadoriaService.findById(id).subscribe((response: ResponseApi)=>{
+            if(response != null){
+                if(response.data != null){
+                    this.item = response.data;
+                }else{
+                    response.erros.forEach(x => alert(x));
+                }
+            }else{
+                alert('Ocorreu um erro inesperado, tente novamente mais tarde!!!');
+            }
+        });
+    }
 }
